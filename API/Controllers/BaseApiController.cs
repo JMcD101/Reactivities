cc769c5f19f3c7abde991c0616d2436ed193876d
Janus McDonald <janusmcd101@gmail.com>
2022-04-21 10:00:26 +0200

    End of Section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,5 +1,7 @@
 using System;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -8,6 +10,8 @@ namespace API.Controllers
 
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
 
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
